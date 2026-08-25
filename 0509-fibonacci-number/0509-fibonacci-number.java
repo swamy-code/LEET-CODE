4d0d3866ca79229b4b8b983e32cfae5c61f25973
BUDDEPU VENKATA PANCHAMUKESHWARA SWAMY <bvpmswamy@gmail.com>
2026-08-25 22:40:08 +0530

    Time: 0 ms (100%), Space: 42.4 MB (16.04%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int fib(int n) {
+        int a=0,b=1,c=0;
+        if(n==0){
+            return a;
+        }
+        else if(n==1){
+            return b;
+        }
+        else{
+            for(int i=2;i<=n;i++){
+                c=a+b;
+                a=b;
+                b=c;
+            }
+            
+        }
+
+         return c;
+    }
+    
+}
